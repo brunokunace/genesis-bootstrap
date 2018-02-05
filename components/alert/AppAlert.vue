bc329b48910b0fbd8fa6f79a7ec3bc746adf1e33
Bruno Henry <brunohenryt@hotmail.com>
2018-02-05 21:57:34 -0200

    Initial Commit

---
+++ components/alert/AppAlert.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <b-alert v-bind="{show, variant, dismissible, dismissLabel}">
+            <slot/>
+        </b-alert>
+    </div>
+</template>
+
+<script>
+    export default {
+        extends: {},
+        mixins: [],
+        name: 'app-alert',
+        props: {
+            show: {
+                type: [Boolean, Number],
+                default: () => false
+            },
+            variant: {
+                type: String,
+                default: () => 'info'
+            },
+            dismissible: {
+                type: Boolean,
+                default: () => false
+            },
+            dismissLabel: {
+                type: String,
+                default: () => 'Close'
+            },
+        }
+    }
+</script>
